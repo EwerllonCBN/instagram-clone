cfe7fc34217b623c0e6ce624405c0dfea47397b9
ewerlloncbn <ewerllon_cristian@hotmail.com>
2024-01-06 22:52:10 -0400

    :style: refactor - size image defined in the Header page

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -5,8 +5,13 @@ import { Logo } from '../assets/logo-white.png'
 export function Header() {
   return (
     <HStack pt={5}>
-      <Image alt="logo" source={require('../assets/logo-white.png')} />
-      <Text color="white">Name</Text>
+      <Image
+        alt="logo"
+        resizeMode="contain"
+        width={100}
+        height={50}
+        source={require('../assets/logo-white.png')}
+      />
     </HStack>
   )
 }
