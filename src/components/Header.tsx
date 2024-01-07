073b86facc76acbf34de9cef1e62f3932e5fc106
ewerlloncbn <ewerllon_cristian@hotmail.com>
2024-01-07 13:49:50 -0400

    :sparkles: feat - added facebook message and liked icons in the project instagram clone

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,17 +1,47 @@
 import { HStack, Image, Stack, Text, VStack } from 'native-base'
 import React from 'react'
 import { Logo } from '../assets/logo-white.png'
+import { Touchable, TouchableOpacity } from 'react-native'
 
 export function Header() {
   return (
-    <HStack pt={5}>
-      <Image
-        alt="logo"
-        resizeMode="contain"
-        width={100}
-        height={50}
-        source={require('../assets/logo-white.png')}
-      />
+    <HStack
+      pt={5}
+      paddingX={5}
+      flexDirection="row"
+      alignItems="center"
+      justifyContent="space-between"
+    >
+      <TouchableOpacity>
+        <Image
+          alt="logo"
+          resizeMode="contain"
+          width={100}
+          height={50}
+          source={require('../assets/logo-white.png')}
+        />
+      </TouchableOpacity>
+
+      <Stack flexDirection="row" style={{ gap: 10 }}>
+        <TouchableOpacity>
+          <Image
+            alt="like"
+            resizeMode="contain"
+            width={30}
+            height={30}
+            source={require('../assets/like-white.png')}
+          />
+        </TouchableOpacity>
+        <TouchableOpacity>
+          <Image
+            alt="like"
+            resizeMode="contain"
+            width={30}
+            height={30}
+            source={require('../assets/messenger-white.png')}
+          />
+        </TouchableOpacity>
+      </Stack>
     </HStack>
   )
 }
