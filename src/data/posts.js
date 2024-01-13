d83f895cbb6d87acab6480a6fa9e4bd2f8272b6e
ewerlloncbn <ewerllon_cristian@hotmail.com>
2024-01-13 13:00:50 -0400

    :sparkles: feat - create posts json file for consumed mock data!

---
+++ src/data/posts.js
@@ -0,0 +1,36 @@
+import { USERS } from './users'
+
+export const POSTS = [
+  {
+    imageUrl: '',
+    user: USERS[0].user,
+    likes: 'Foto das férias no Caribe',
+    profile_picture: USERS[0].image,
+    coments: [
+      {
+        user: 'deboraseco_',
+        coment: 'Que lugar fantástico! Quero muito conhecer.'
+      },
+      {
+        user: 'neymarjr',
+        coment: 'Vai, ta por onde?... Olha teu wpp!'
+      }
+    ]
+  },
+  {
+    imageUrl: '',
+    user: USERS[0].user,
+    likes: 'Foto das férias no Caribe',
+    profile_picture: USERS[0].image,
+    coments: [
+      {
+        user: 'deboraseco_',
+        coment: 'Que lugar fantástico! Quero muito conhecer.'
+      },
+      {
+        user: 'neymarjr',
+        coment: 'Vai, ta por onde?... Olha teu wpp!'
+      }
+    ]
+  }
+]
